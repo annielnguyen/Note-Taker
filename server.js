a5da1a0da0527004e6ca3a2b649f2fa9a0690d4b
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-16 23:38:38 -0800

    add back API route

--- server.js
+++ server.js
@@ -13,14 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Add a static middleware for serving assets in the public folder
-app.use(express.static("./develop/public"));
+app.use(express.static("Develop/public"));
 
 // Async processes
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-//API post requests
+// API Route | "GET" request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+      })
+  }); 
 
+//API post requests
 app.post("/api/notes", function(req, res) {
     const note = req.body;
     readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
@@ -29,7 +36,7 @@ app.post("/api/notes", function(req, res) {
       notes.push(note);
       return notes
     }).then(function(notes) {
-      writeFileAsync("./Develop/db/db.json", JSON.stringify(notes))
+      writeFileAsync("Develop/db/db.json", JSON.stringify(notes))
       res.json(note);
     })
   });
@@ -49,5 +56,5 @@ app.get("/notes", function(req, res) {
 
 // Listen for connections
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("Example app listening at PORT " + PORT);
   });
