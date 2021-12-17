a488bc4c81e42ce386402fe3f99e0d60512efbed
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-16 21:28:31 -0800

    added api route get request

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ app.use(express.json());
 
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('public'));
+
+//API route get request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+      })
+  }); 
+  
