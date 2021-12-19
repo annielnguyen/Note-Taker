5962342e86c485a9635c0927db1205fe6735184b
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-18 22:52:40 -0800

    added functioning delete note code and added readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Note-Taker
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Project Name: Note-Taker
+https://github.com/annielnguyen/Note-Taker
+## Description
+This application is used to write and save notes. It uses Express.js back end and saves and retrieves note data from a JSON file.
+## Table of Contents
+  
+* [License](#license)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+  
+
+## License
+This project is licensed under the MIT license. 
+  
+## Installation
+  Please install these dependencies to run this application: Express.js
+  
+## Usage
+  To use this app, Please run node server.js in your terminal and type http://localhost:3001/ into your browser.
+  From there, click the "Get Started" button and enter the note title and text, and then click the save icon in the upper right hand corner. View my demo here: https://watch.screencastify.com/v/DFESOqRBBOeGblBYGF7S
+  ![Screenshot1](/Develop/public/assets/ss1.png)
+  ![Screenshot2](/Develop/public/assets/ss2.png)
+## Contribution
+  ​Contributors: Annie Nguyen
+
+## Tests
+  The following is needed to run the test: node server.js
+
+## Questions
+  My GitHub profile link is: https://github.com/annielnguyen.
+  If you have any questions regarding this app, please contact directly at: anguyen.aln@gmail.com.
+  
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-const allNotes = require('./Develop/db/db.json');
+const allNotes = require("./Develop/db/db.json");
 
 //set up server
 const PORT = process.env.PORT || 3001;
@@ -14,7 +14,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Add a static middleware for serving assets in the public folder
 app.use(express.static("./Develop/public"));
 
@@ -23,63 +22,71 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // API Route | "GET" request
-app.get("/api/notes", function(req, res) {
-  readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-      })
-  }); 
+app.get("/api/notes", function (req, res) {
+  readFileAsync("./Develop/db/db.json", "utf8").then(function (data) {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  });
+});
 
 //API post requests
-app.post("/api/notes", function(req, res) {
-    const note = req.body;
-    readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
+app.post("/api/notes", function (req, res) {
+  const note = req.body;
+  readFileAsync("./Develop/db/db.json", "utf8")
+    .then(function (data) {
       const notes = [].concat(JSON.parse(data));
-      note.id = notes.length + 1
+      note.id = notes.length + 1;
       notes.push(note);
-      return notes
-    }).then(function(notes) {
-      writeFileAsync("./Develop/db/db.json", JSON.stringify(notes))
-      res.json(note);
+      return notes;
     })
-  });
+    .then(function (notes) {
+      writeFileAsync("./Develop/db/db.json", JSON.stringify(notes));
+      res.json(note);
+    });
+});
 
 // HTML Routes
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
-    });
-  
-  app.get("/", function(req, res) {
-       res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-    });
-  
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-   });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
 
-   //delete note
-   function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './Develop/db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
-            break;
-        }
+//delete note
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+    console.log(note);
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+      // fs.writeFileSync(
+      //     path.join(__dirname, './Develop/db/db.json'),
+      //     JSON.stringify(notesArray, null, 2)
+
+      // );
+      writeFileAsync("./Develop/db/db.json", JSON.stringify(notesArray));
+      break;
     }
+  }
+  return notesArray;
 }
 
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
+app.delete("/api/notes/:id", (req, res) => {
+  readFileAsync("./Develop/db/db.json", "utf8").then(function (data) {
+    console.log(data);
+    notesArray = deleteNote(req.params.id, JSON.parse(data));
+    console.log(notesArray);
+    res.json(notesArray);
+  });
 });
 
 // Listen for connections
-  app.listen(PORT, function() {
-    console.log("Example app listening at PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("Example app listening at PORT " + PORT);
+});
