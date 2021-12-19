fb2644ae6e6da412b48eb3ac5ebb5cc25b908034
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-18 20:40:12 -0800

    added delete code

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
+const allNotes = require('./Develop/db/db.json');
+
 //set up server
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,8 +14,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Add a static middleware for serving assets in the public folder
-app.use(express.static("Develop/public"));
+app.use(express.static("./Develop/public"));
 
 // Async processes
 const readFileAsync = util.promisify(fs.readFile);
@@ -21,7 +24,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 // API Route | "GET" request
 app.get("/api/notes", function(req, res) {
-    readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
+  readFileAsync("./Develop/db/db.json", "utf8").then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
       })
@@ -36,23 +39,45 @@ app.post("/api/notes", function(req, res) {
       notes.push(note);
       return notes
     }).then(function(notes) {
-      writeFileAsync("Develop/db/db.json", JSON.stringify(notes))
+      writeFileAsync("./Develop/db/db.json", JSON.stringify(notes))
       res.json(note);
     })
   });
 
 // HTML Routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
     });
   
   app.get("/", function(req, res) {
-       res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+       res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
     });
   
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+      res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
    });
+
+   //delete note
+   function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './Develop/db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
 
 // Listen for connections
   app.listen(PORT, function() {
